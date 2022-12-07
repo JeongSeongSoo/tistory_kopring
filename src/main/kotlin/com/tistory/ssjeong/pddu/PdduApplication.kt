01d61c61c161e7a6410d829b6d85577c0a8892ee
ssjeong <ssjeong920801@gmail.com>
2022-12-07 13:12:40 +0900

    [Kotlin] 코틀린의 반복문, 조건문, 선택문

---
+++ src/main/kotlin/com/tistory/ssjeong/pddu/PdduApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.ssjeong.pddu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdduApplication
+
+fun main(args: Array<String>) {
+	runApplication<PdduApplication>(*args)
+}
---
+++ src/test/kotlin/com/tistory/ssjeong/pddu/PdduApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.ssjeong.pddu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PdduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
